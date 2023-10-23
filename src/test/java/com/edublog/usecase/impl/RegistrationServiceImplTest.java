3e7505726398d0f524de1bb6fd91ba9be2f8120c
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2023-10-23 00:17:03 -0300

    Revert "Updated service and adapter"
    This reverts commit 2e3365f64c853c93eab302ea5d98c4a541a0b89a.

---
+++ src/test/java/com/edublog/usecase/impl/RegistrationServiceImplTest.java
@@ -0,0 +1,14 @@
+package com.edublog.usecase.impl;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceImplTest {
+
+    @BeforeAll
+    void setUp() {
+        
+    }
+
+}
