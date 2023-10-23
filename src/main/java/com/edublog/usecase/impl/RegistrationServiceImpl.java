4caadcaaa98ca586e4f4b6d0257eb51dd3f1aa70
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2023-10-23 00:17:29 -0300

    Revert "Revert "Revert "Updated service and adapter"""
    This reverts commit 4204244f4c1b70ea24174176ec8018e82e913486.

--- src/main/java/com/edublog/usecase/impl/RegistrationServiceImpl.java
+++ src/main/java/com/edublog/usecase/impl/RegistrationServiceImpl.java
@@ -16,6 +16,7 @@ import org.springframework.stereotype.Service;
 public class RegistrationServiceImpl implements RegistrationService {
 
     private final AccountRepository accountRepository;
+    private final AccountAdapter accountAdapter = AccountAdapter.INSTANCE;
     private final PasswordEncoder passwordEncoder;
 
     @Override
