c17c934646cf31be74322d62b8c3ee11a46e4a93
Eduardo Fontenele <eduardo.fontenaille@gmail.com>
2023-10-26 23:05:19 -0300

    fodase

--- src/main/java/com/edublog/config/BlogSecurityConfig.java
+++ src/main/java/com/edublog/config/BlogSecurityConfig.java
@@ -5,12 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.DefaultSecurityFilterChain;
 
 @Configuration
@@ -20,7 +16,7 @@ public class BlogSecurityConfig {
     DefaultSecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
         http.authorizeHttpRequests(req -> req
                 .requestMatchers("/register").permitAll()
-                .requestMatchers("/publications/*", "/principal/*").authenticated()
+                .requestMatchers("/publications/*", "/principal/*"   ).authenticated()
         );
         http.csrf(AbstractHttpConfigurer::disable);
         http.formLogin(Customizer.withDefaults());
